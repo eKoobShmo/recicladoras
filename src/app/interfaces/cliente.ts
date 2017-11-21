ba0ceabc69a76740f297cdb22f2f5e82cb814c6a
David <liodavid0@gmail.com>
2017-11-21 13:25:17 -0700

    Commit para hacer rebase

---
+++ src/app/interfaces/cliente.ts
@@ -0,0 +1,14 @@
+export interface Cliente {
+    nombre: string;
+    apellidoPaterno: string;
+    apellidoMaterno: string;
+    rfc: string;
+    mesComprobante: string;
+    identificacionUrl: string;
+    comprobanteDomicilioUrl: string;
+    huellaIzquierdaUrl: string;
+    huellaDerechaUrl: string;
+    fotoUrl: string;
+
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,24 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
+import {Cliente} from "../../interfaces/cliente";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    clientes: FirebaseListObservable<Cliente[]>;
 
 
-  mostrardatos(){
-    console.log("ee asd d")
-  }
+    constructor(private db: AngularFireDatabase,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-  }
 
-  enviarComentario(){
-    console.log("hsdf");
+    ngOnInit() {
+        this.clientes = this.db.list('Clientes')
+
+
+
+    }
+
+    verCliente(key: string) {
+        this.router.navigate(['/cliente', key])
+    }
+
+    agregarCliente(){
+        this.clientes.push({
+            nombre: "Pancho",
+            apellidoPaterno: "Alcaraz",
+            apellidoMaterno: "Perez",
+            rfc: "fdsafasd",
+            mesComprobante: "Enero",
+            identificacionUrl: "asdfasdf",
+            comprobanteDomicilioUrl: "dasfsdf",
+            huellaIzquierdaUrl: "asdfas",
+            huellaDerechaUrl: "adfssd",
+            fotoUrl: "https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/user_male2-512.png"
+        })
   }
 
 }
+
