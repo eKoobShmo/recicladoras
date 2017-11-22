c0341c262bb97ed7cc1fc17c653b1b6234eb1e76
David <liodavid0@gmail.com>
2017-11-22 11:10:32 -0700

    Se agrego la funcion de buscar productos

--- src/app/pages/precios/precios.component.ts
+++ src/app/pages/precios/precios.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
 import {Producto} from "../../interfaces/producto";
 
 @Component({
-  selector: 'app-precios',
-  templateUrl: './precios.component.html',
-  styleUrls: ['./precios.component.scss']
+    selector: 'app-precios',
+    templateUrl: './precios.component.html',
+    styleUrls: ['./precios.component.scss']
 })
 export class PreciosComponent implements OnInit {
 
-    productos:FirebaseListObservable<Producto[]>;
+    productos: Producto[];
+    productosTemporal: Producto[];
+    busqueda: string;
 
-  constructor(private db: AngularFireDatabase) { }
+    constructor(private db: AngularFireDatabase) {
+    }
 
-  ngOnInit() {
-    this.productos = this.db.list('Productos')
-  }
-  
+    ngOnInit() {
+        this.db.list('Productos').subscribe(result => {
+            this.productos =  this.productosTemporal = result;
+        });
+    }
+
+    searchProduct(terminoBusqueda: string) {
+        if (!this.productos) {
+            this.productosTemporal = []
+        } else {
+            this.productosTemporal = this.productos.filter(it => it.producto.toLowerCase().indexOf(terminoBusqueda.toLowerCase()) >= 0);
+        }
+    }
+
+
 }
