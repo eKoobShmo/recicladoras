c75462396cbd1b5c071ba0b0d89fbab8f640e606
David <liodavid0@gmail.com>
2017-11-23 11:02:43 -0700

    Se agrego funcion para mostrar lista de productos

--- src/app/interfaces/producto.ts
+++ src/app/interfaces/producto.ts
@@ -1,2 +1,7 @@
 export interface Producto {
+    nombre:string;
+    unidad:string;
+    precio:number;
+    cantidad:number;
+    total:number;
 }
--- src/app/pages/venta/venta.component.ts
+++ src/app/pages/venta/venta.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Producto} from "../../interfaces/producto";
 
 @Component({
-  selector: 'app-venta',
-  templateUrl: './venta.component.html',
-  styleUrls: ['./venta.component.scss']
+    selector: 'app-venta',
+    templateUrl: './venta.component.html',
+    styleUrls: ['./venta.component.scss']
 })
 export class VentaComponent implements OnInit {
 
-  constructor() { }
+    productos: Producto[] = [
+        {
+            nombre: "Puerta Fierro",
+            unidad: "Unidad",
+            precio: 25,
+            cantidad: 2,
+            total: 50,
+        },
+        {
+            nombre: "Puerta Fierro",
+            unidad: "Unidad",
+            precio: 25,
+            cantidad: 2,
+            total: 50,
+        },
+        {
+            nombre: "Puerta Fierro",
+            unidad: "Unidad",
+            precio: 25,
+            cantidad: 2,
+            total: 50,
+        },
+        {
+            nombre: "Puerta Fierro",
+            unidad: "Unidad",
+            precio: 25,
+            cantidad: 2,
+            total: 50,
+        },
+        {
+            nombre: "Puerta Fierro",
+            unidad: "Unidad",
+            precio: 25,
+            cantidad: 2,
+            total: 50,
+        },
+        {
+            nombre: "Puerta Fierro",
+            unidad: "Unidad",
+            precio: 25,
+            cantidad: 2,
+            total: 50,
+        },
+        {
+            nombre: "Puerta Fierro",
+            unidad: "Unidad",
+            precio: 25,
+            cantidad: 2,
+            total: 50,
+        },
 
-  ngOnInit() {
-  }
+    ];
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
