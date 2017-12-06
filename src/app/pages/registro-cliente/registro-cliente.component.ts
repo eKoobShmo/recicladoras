6b6ca97077a5e7fd945cb2da428b9e17f4d8a107
Hector Manjarrez <hmanjarrez@ekoobs.com.mx>
2017-12-05 19:11:14 -0700

    .

--- src/app/pages/registro-cliente/registro-cliente.component.ts
+++ src/app/pages/registro-cliente/registro-cliente.component.ts
@@ -9,8 +9,20 @@ export class RegistroClienteComponent implements OnInit {
 
 dropzonePostUrl:any = 'dropzonePostUrl'
 dropzonePostUrl1:any = 'dropzonePostUrl1'
+public nombres:string;
+public app:string;
+public apm:string;
+public rfc:string;
+public ine:File;
+public comp:File;
+public fpl:string;
+public fpr:string;
+public upload:boolean;
+public progress:number=0;
 
-  constructor() { }
+constructor( archivo:File ) {
+
+  }
 
   ngOnInit() {
   }
